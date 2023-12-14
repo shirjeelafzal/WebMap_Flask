d4c2f21c7a6ed78d74408ee59587fb0434d90d49
Shirjeel Afzal <shirjeelafzals@gmail.com>
2023-12-14 18:43:52 +0500

    added auth

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,EmailField
+from wtforms import StringField, PasswordField, SubmitField,EmailField,FloatField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from geoalchemy2.types import Geometry
 from flask_migrate import Migrate
 from sqlalchemy import ForeignKey
+from flask import flash
 from sqlalchemy.orm import relationship
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
@@ -14,6 +15,9 @@ from flask_bootstrap import Bootstrap
 from wtforms import StringField, IntegerField, DateTimeField,DateField
 from wtforms.validators import DataRequired, Length, Optional
 from sqlalchemy.exc import SQLAlchemyError
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import Admin
+from flask_login import UserMixin ,login_user,LoginManager,login_required,logout_user,current_user
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:tsatsatsa9@localhost:5432/actualdatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,9 +25,23 @@ app.config['SECRET_KEY'] = os.urandom(24)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 Bootstrap(app)
+admin=Admin(app)
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return user_model.query.get(int(user_id))
 
 
-
+class user_model(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    first_name=db.Column(db.String(100),nullable=False)
+    last_name=db.Column(db.String(100),nullable=False)
+    email=db.Column(db.String(255), nullable=False, unique=True)
+    password=db.Column(db.String(255), nullable=False)
+    
 class species_model(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     scientific_name = db.Column(db.String(100), nullable=False)
@@ -40,7 +58,7 @@ class trees_model(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     location = db.Column(Geometry(geometry_type='POINT', srid=4326))
     planting_date = db.Column(db.Date)
-    state_of_conservation = db.Column(db.String(80), unique=True, nullable=False)
+    state_of_conservation = db.Column(db.String(80), nullable=False)
     height=db.Column(db.Integer)
     diameter=db.Column(db.Integer)
     last_pruning=db.Column(db.Date)
@@ -62,7 +80,27 @@ class gardeners_model(db.Model):
     trees = relationship('trees_model', back_populates='gardener')
     def __repr__(self):
         return f'{self.id} - {self.name}'
+
+
+# def TreesView(BaseModelView):
+   
+#     column_list =["location","planting_date","state_of_conservation","height","diameter","last_pruning","gardener","species"]
+####################Registering models in admin##############
+admin.add_view(ModelView(user_model, db.session,name='User'))
+admin.add_view(ModelView(trees_model,db.session,name='Trees'))
+admin.add_view(ModelView(species_model,db.session,name='Species'))
+admin.add_view(ModelView(gardeners_model,db.session,name='Gardeners'))
 #################forms####################
+class SignupForm(FlaskForm):
+    class Meta:
+        model=user_model
+    first_name=StringField('First Name', validators=[DataRequired(), Length(max=100)])
+    last_name=StringField('Last Name', validators=[DataRequired(), Length(max=100)])
+    email=EmailField('Email', validators=[DataRequired(), Length(max=100)])
+    password=PasswordField('Password', validators=[DataRequired(), Length(max=100)])
+class LoginForm(FlaskForm):
+    email=EmailField('Email', validators=[DataRequired(), Length(max=100)])
+    password=PasswordField('Password', validators=[DataRequired(), Length(max=100)])
 class SpeciesForm(FlaskForm):
     class Meta:
         model = species_model
@@ -77,15 +115,75 @@ class SpeciesForm(FlaskForm):
 class GardenersForm(FlaskForm):
     class Meta:
         model=gardeners_model
-    name = StringField('name', validators=[DataRequired(), Length(max=100)])
-    surname = StringField('surname', validators=[DataRequired(), Length(max=100)])
-    email = EmailField('email', validators=[DataRequired(), Length(max=100)])
+    name = StringField('Name', validators=[DataRequired(), Length(max=100)])
+    surname = StringField('Surname', validators=[DataRequired(), Length(max=100)])
+    email = EmailField('Email', validators=[DataRequired(), Length(max=100)])
+
+class TreesForm(FlaskForm):
+    class Meta:
+        model=trees_model
+    location_x = FloatField('Location X', validators=[DataRequired()])
+    location_y = FloatField('Location Y', validators=[DataRequired()])
+    planting_date = DateField('Planting Date', validators=[DataRequired()])
+    state_of_conservation = StringField('State of Conservation', validators=[DataRequired(), Length(max=100)])
+    height = IntegerField('Height', validators=[DataRequired()])
+    diameter = IntegerField('Diameter', validators=[DataRequired()])
+    last_pruning = DateField('Last Pruning', validators=[DataRequired()])
+    gardener_id=IntegerField('Gardener',validators=[DataRequired()])
+    species_id=IntegerField('Species',validators=[DataRequired()])
 #################################adding routes######################
 @app.route('/')
-def hello_world():
+def dashboard():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=user_model.query.filter_by(email=form.email.data).first()
+        if user:
+            #check the password
+            if user.password==form.password.data:
+                login_user(user)
+                flash('Login Successful')
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Wrong  Password - Try again")
+                redirect(url_for('login'))
+        else:
+            flash("Email is incorrect")
+            redirect(url_for('login'))
+    return render_template('login.html',form=form)
+@app.route('/logout',methods=['GET','POST'])
+@login_required
+def logout():
+    logout_user()
+    flash('Logout Successful')
+    return redirect(url_for('login'))
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form =SignupForm()
+    try:
+        if form.validate_on_submit():
+            # Create a new species instance and add it to the database
+            model_data = {
+                'first_name': form.first_name.data,
+                'last_name': form.last_name.data,
+                'email': form.email.data,
+                'password': form.password.data,               
+            }
+            new_species = user_model(**model_data)
+            db.session.add(new_species)
+            db.session.commit()
+            return 'User created successfully!'
+    except SQLAlchemyError as e:
+        db.session.rollback()  # Roll back the changes made in the current session
+        print(f"Error: {e}")
+        return 'Error creating user. Please try again.'
+    return render_template('signup.html', form=form)
 @app.route('/species', methods=['GET', 'POST'])
-def create_species():
+@login_required
+def species():
     form = SpeciesForm()
     try:
         if form.validate_on_submit():
@@ -101,93 +199,99 @@ def create_species():
             new_species = species_model(**model_data)
             db.session.add(new_species)
             db.session.commit()
-            return 'Species created successfully!'
+            flash('Form Submitted Successfully')
+            return redirect(url_for("species"))
     except SQLAlchemyError as e:
         db.session.rollback()  # Roll back the changes made in the current session
         print(f"Error: {e}")
         return 'Error creating species. Please try again.'
-    return render_template('species.html', form=form)
+    species=species_model.query.all()
+    return render_template('species.html', form=form,species=species)
 
-@app.route('/gardeners', methods=['GET', 'POST'])
-def create_gardeners():
+@app.route('/gardeners', methods=['GET', 'POST','DELETE','PUT'])
+def gardeners():
     form = GardenersForm()
+    if request.method=='POST':
+        try:
+            if form.validate_on_submit():
+                model_data = {
+                    'name': form.name.data,
+                    'surname': form.surname.data,
+                    'email': form.email.data,
+                }
+                new_gardener = gardeners_model(**model_data)
+                db.session.add(new_gardener)
+                db.session.commit()
+                flash('Form Submitted Successfully.')
+                return redirect(url_for('gardeners'))
+        except SQLAlchemyError as e:
+            db.session.rollback()  # Roll back the changes made in the current session
+            print(f"Error: {e}")
+            flash('Error creating gardener. Please try again.')
+            return redirect(url_for('gardeners'))
+    elif request.method=='DELETE':
+        gardener_id_to_delete=request.args.get('gardener_id')
+        if gardener_id_to_delete:
+            try:
+                gardener=gardeners_model.query.get(gardener_id_to_delete)
+                if gardener:
+                    db.session.delete(gardener)
+                    db.session.commit()
+                    flash('Form Deleted Successfully.')
+                    return redirect(url_for('gardeners'))
+            except SQLAlchemyError as e:
+                db.session.rollback()
+                return 'Error deleting gardener. Please try again.'
+
+    elif request.method=='PUT':
+        gardener_id_to_update=request.args.get('gardener_id')
+        if gardener_id_to_update:
+            try:
+                gardener=gardeners_model.query.get(gardener_id_to_update)
+                if gardener:
+                    gardener.name=form.name.data
+                    gardener.surname = form.surname.data
+                    gardener.email = form.email.data
+                    db.session.commit()
+            except SQLAlchemyError as e:
+                db.session.rollback()
+                return 'Error updating gardener. Please try again.'
+    gardeners= gardeners_model.query.all()
+    return render_template('gardeners.html', form=form , gardeners=gardeners)
+
+
+@app.route('/trees', methods=['GET','POST'])
+@login_required
+def trees():
+    form = TreesForm()
     try:
         if form.validate_on_submit():
-            model_data = {
-                'name': form.name.data,
-                'surname': form.surname.data,
-                'email': form.email.data,
+            location_x = float(form['location_x'].data)
+            location_y = float(form['location_y'].data)
+            model_data={
+            'planting_date' : datetime.strptime(str(form['planting_date'].data), '%Y-%m-%d'),
+            'state_of_conservation' : form['state_of_conservation'].data,
+            'height' : int(form['height'].data),
+            'diameter' : int(form['diameter'].data),
+            'last_pruning' : datetime.strptime(str(form['last_pruning'].data), '%Y-%m-%d'),
+            'gardener_id' : int(form['gardener_id'].data),
+            'species_id' : int(form['species_id'].data),
+            'location' : WKTElement(f'POINT({location_x} {location_y})', srid=4326),
             }
-            new_species = gardeners_model(**model_data)
-            db.session.add(new_species)
+            
+            new_tree = trees_model(**model_data)
+            db.session.add(new_tree)
             db.session.commit()
-            return 'Gardeners created successfully!'
+            flash('Form Submitted Successfully')
+            return redirect(url_for('trees'))
     except SQLAlchemyError as e:
         db.session.rollback()  # Roll back the changes made in the current session
         print(f"Error: {e}")
-        return 'Error creating gardener. Please try again.'
-    return render_template('gardeners.html', form=form)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.route('/add_tree', methods=['POST'])
-def add_tree():
-    if request.method == 'POST':
-        location_x = float(request.form['location_x'])
-        location_y = float(request.form['location_y'])
-        planting_date = datetime.strptime(request.form['planting_date'], '%Y-%m-%d')
-        state_of_conservation = request.form['state_of_conservation']
-        height = int(request.form['height'])
-        diameter = int(request.form['diameter'])
-        last_pruning = datetime.strptime(request.form['last_pruning'], '%Y-%m-%d')
-
-        # Create a WKT(well known text) point from the coordinates
-        location = WKTElement(f'POINT({location_x} {location_y})', srid=4326)
-
-        new_tree = trees_model(
-            location=location,
-            planting_date=planting_date,
-            state_of_conservation=state_of_conservation,
-            height=height,
-            diameter=diameter,
-            last_pruning=last_pruning
-        )
-
-        db.session.add(new_tree)
-        db.session.commit()
-
-        return redirect(url_for('some_success_page'))  # Redirect to a success page or another route
-    else:
-        return render_template('add_tree_form.html')  # Render a form to add trees
-
+        return redirect(url_for("trees"))
+    trees=trees_model.query.all()
+    return render_template('trees.html', form=form,trees=trees)
+            
+    
 
 
 
